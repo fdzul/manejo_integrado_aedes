9255276c216419dacb1727790347adfdd9aa9633
fdzul <felipe.dzul.m@gmail.com>
2023-09-27 21:45:18 -0600

    this commit update the talk

--- universo_loc_21.R
+++ universo_loc_21.R
@@ -1,18 +1,10 @@
 
-
-loc <- rgeomex::extract_locality(cve_edo = "21",
-                                 locality = "Xicotepec de Juarez")
-
-
-
 # extract the localities ####
 manz <- rgeomex::blocks_ine20_mx_d |>
     dplyr::filter(entidad == 21)
-table(manz$entidad)
+
 
 # extract the boundary localities #"####
-
-
 puebla <- rgeomex::extract_locality(cve_edo = "21",
                                     locality = c("Heroida Puebla de Zaragoza")) |>
     dplyr::mutate(area_km = sf::st_area(geometry)) |>
@@ -74,6 +66,57 @@ manz_carranza <- manz[v_carranza, ]
 manz_xicotepec <- manz[xicotepec, ]
 
 #
+acatlan <- acatlan |>
+    dplyr::mutate(Manzanas = nrow(manz_acatlan)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+chietla <- chietla |>
+    dplyr::mutate(Manzanas = nrow(manz_chietla)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+izucar <- chietla |>
+    dplyr::mutate(Manzanas = nrow(manz_chietla)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+jalpan <- jalpan |>
+    dplyr::mutate(Manzanas = nrow(manz_jalpan)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+puebla <- puebla |>
+    dplyr::mutate(Manzanas = nrow(manz_puebla)) |>
+    dplyr::mutate(Actividad = "Focalizado") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+tehuacan <- tehuacan |>
+    dplyr::mutate(Manzanas = nrow(manz_tehuacan)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+tepexco <- tepexco |>
+    dplyr::mutate(Manzanas = nrow(manz_tepexco)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+tepexi <- tepexi |>
+    dplyr::mutate(Manzanas = nrow(manz_tepexi)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+v_carranza <- v_carranza |>
+    dplyr::mutate(Manzanas = nrow(manz_carranza)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+xicotepec <- xicotepec |>
+    dplyr::mutate(Manzanas = nrow(manz_xicotepec)) |>
+    dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::select(NOMGEO, ha, Manzanas, Actividad)
+
+
 acatlan$manzanas <- nrow(manz_acatlan)
 chietla$manzanas <- nrow(manz_chietla)
 izucar$manzanas <- nrow(manz_izucar)
@@ -86,24 +129,73 @@ v_carranza$manzanas <- nrow(manz_carranza)
 xicotepec$manzanas <- nrow(manz_xicotepec)
 
 
-dplyr::bind_rows(acatlan |> dplyr::select(NOMGEO, ha, manzanas),
-                 chietla |> dplyr::select(NOMGEO, ha, manzanas),
-                 izucar |> dplyr::select(NOMGEO, ha, manzanas),
-                 jalpan |> dplyr::select(NOMGEO, ha, manzanas),
-                 puebla |> dplyr::select(NOMGEO, ha, manzanas),
-                 tehuacan |> dplyr::select(NOMGEO, ha, manzanas),
-                 tepexco |> dplyr::select(NOMGEO, ha, manzanas),
-                 tepexi |> dplyr::select(NOMGEO, ha, manzanas),
-                 v_carranza |> dplyr::select(NOMGEO, ha, manzanas),
-                 xicotepec |> dplyr::select(NOMGEO, ha, manzanas)) |>
+dplyr::bind_rows(acatlan,
+                 chietla,
+                 izucar,
+                 jalpan,
+                 puebla,
+                 tehuacan,
+                 tepexco,
+                 tepexi,
+                 v_carranza,
+                 xicotepec) |>
     sf::st_drop_geometry() |>
-    dplyr::mutate(Actividad = "Barrido") |>
+    #dplyr::mutate(Actividad = "Barrido") |>
     dplyr::mutate(Ciclos = 4) |>
-    dplyr::mutate("Fecha Inicio" = "20023-09-11") |>
-    dplyr::mutate("Fecha Termino" = "20023-10-13") |>
-    dplyr::rename(Manzanas = manzanas,
-                  Hectareas = ha,
+    dplyr::mutate("Fecha Inicio" = "NA") |>
+    dplyr::mutate("Fecha Termino" = "NA") |>
+    dplyr::rename(Hectareas = ha,
                   Localidad = NOMGEO) |>
     dplyr::mutate(Hectareas = round(Hectareas, 0)) |>
+    reactable::reactable(theme = reactablefmtr::flatly(),
+                         filterable = TRUE)
+
+
+loc <- dplyr::bind_rows(acatlan,
+                        chietla,
+                        izucar,
+                        jalpan,
+                        puebla,
+                        tehuacan,
+                        tepexco,
+                        tepexi,
+                        v_carranza,
+                        xicotepec) |>
+    sf::st_drop_geometry() |>
+    #dplyr::mutate(Actividad = "Barrido") |>
+    dplyr::mutate(Ciclos = 4) |>
+    dplyr::mutate("Fecha Inicio" = "NA") |>
+    dplyr::mutate("Fecha Termino" = "NA") |>
+    dplyr::rename(Hectareas = ha,
+                  Localidad = NOMGEO) |>
+    dplyr::mutate(Hectareas = round(Hectareas, 0))
+
+
+loc |>
+    dplyr::mutate("Garrafas 1 Ciclo" = round((Hectareas * 260)/20000,
+                                   digits = 0)) |>
+    dplyr::mutate("Garrafas 4 Ciclos" = round((Hectareas * 260)/20000,
+                                             digits = 0)*4) |>
+    
+    reactable::reactable(theme = reactablefmtr::flatly(),
+                         filterable = TRUE)
+
+
+loc |>
+    dplyr::select(Localidad, Hectareas, Manzanas,
+                  Actividad, Ciclos) |>
+    dplyr::mutate("Garrafas 1 Ciclo" = round((Hectareas * 260)/20000,
+                                             digits = 0)) |>
+    dplyr::mutate("Garrafas 4 Ciclos" = round((Hectareas * 260)/20000,
+                                              digits = 0)*4) |>
+    
+    reactable::reactable(theme = reactablefmtr::flatly(),
+                         filterable = TRUE)
+
+
+loc |>
+    dplyr::select(Localidad, Hectareas, Manzanas,
+                  Actividad, Ciclos) |>
+    dplyr::mutate("Vehiculo&Maquina" = round((Hectareas/60)/5, 0)) |>
     reactable::reactable(theme = reactablefmtr::flatly(),
                          filterable = TRUE)
